30b29dda91d2a7c56c00a6ea6128b70b2d9b5d01
gzw2060 <1224199208@qq.com>
2018-05-21 11:17:41 +0800

    !

--- webapp/src/main/java/com/jk/controller/gzw/GzwController.java
+++ webapp/src/main/java/com/jk/controller/gzw/GzwController.java
@@ -2,8 +2,17 @@ package com.jk.controller.gzw;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/GzwController")
 public class GzwController {
+
+    @RequestMapping("/getInfoAll")
+    @ResponseBody
+    public Map<String, Object> getInfoAll(){
+        return null;
+    }
 }
