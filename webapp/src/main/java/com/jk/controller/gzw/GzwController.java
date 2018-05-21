72eb1fefb699832851de7c15fa106eed15fe27d0
gzw2060 <1224199208@qq.com>
2018-05-21 10:59:49 +0800

    !

---
+++ webapp/src/main/java/com/jk/controller/gzw/GzwController.java
@@ -0,0 +1,9 @@
+package com.jk.controller.gzw;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/GzwController")
+public class GzwController {
+}
